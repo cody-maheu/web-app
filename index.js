50b421ab034b3008e2f2fb1c91882f75c32193e7
cody-maheu <cmaheu@ucsd.edu>
2021-04-26 08:31:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -81,7 +81,10 @@ app.get("/user", requiresAuth(), async (req, res) => {
 app.get("/expenses", requiresAuth(), async (req, res, next) => {
  try {
    // 👇 get the token from the request 👇
-   const { token_type, access_token } = req.oidc.accessToken;
+   const { token_type, access_token, isExpired, refresh } = req.oidc.accessToken;
+   if (isExpired()) {
+    ({ access_token } = await refresh());
+   }
    // 👇 then send it as an authorization header 👇
    const expenses = await axios.get(`${API_URL}/reports`, {
      headers: {
